31083295d7c9a8e28f16f5eb9bc029add58daf43
Marce <marceross@gmail.com>
2023-12-26 12:34:37 -0300

    Schedule and reservation done. Activity from Admin

--- project5/booking/admin.py
+++ project5/booking/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
-from booking.models import User, UserProfile, Activity
+from booking.models import User, UserProfile, Activity, ActivitySchedule, ActivityReservation
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(UserProfile)
 admin.site.register(Activity)
-
+admin.site.register(ActivitySchedule)
+admin.site.register(ActivityReservation)
 
 #, ActivityDays, ActivitySchedules, ActivityAttendance, ActivityScheduleSuspension, ActivityReservations, ActivityAutoReservation
 
